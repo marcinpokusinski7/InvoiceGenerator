d0a0b49fca5cb1b8beecbcba32fe46a4d01de445
marcinpokusinski7 <marcinpokusinski@gmail.com>
2021-04-10 04:07:07 +0200

    upgraded invoice generator

--- src/main/java/com/vaadin/invoice/ui/views/InvoiceGenerator.java
+++ src/main/java/com/vaadin/invoice/ui/views/InvoiceGenerator.java
@@ -1,14 +1,20 @@
 package com.vaadin.invoice.ui.views;
 
+import com.itextpdf.text.Document;
 import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.datepicker.DatePicker;
 import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.grid.GridVariant;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Footer;
+import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.radiobutton.RadioButtonGroup;
@@ -21,45 +27,61 @@ import com.vaadin.invoice.entity.Product;
 import com.vaadin.invoice.service.ProductService;
 import com.vaadin.invoice.ui.MainLayout;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
+import java.time.LocalDate;
+import java.util.*;
 
 @Route(value = "create", layout = MainLayout.class)
 @CssImport("./styles/shared-styles.css")
 @CssImport(value = "./styles/vaadin-text-field-styles.css", themeFor = "vaadin-text-field")
 public class InvoiceGenerator extends VerticalLayout {
     private ProductService productService;
+    H1 invoiceFvNumber = new H1();
     Map<Integer, Double> values = new TreeMap<Integer, Double>();
     Map<Integer, Double> totalPriceMap = new TreeMap<Integer, Double>();
     List<Integer> productList = new ArrayList<Integer>();
-
     TextField searchItems = new TextField();
-    TextField preparedBy = new TextField("Full name");
+    TextField preparedBy = new TextField("Prepared By");
+    TextField companyLocation = new TextField("Company postal address");
+    TextField identificationNumberPreparedBy = new TextField("Identification Number");
     TextField clientName = new TextField("Client Full name");
-    TextField IdentificationNumber = new TextField("Client Full name");
+    TextField companyLocationOClient = new TextField("Company postal address");
+    TextField identificationNumber = new TextField("Identification Number");
     DatePicker dateOfPreparation = new DatePicker("Transaction Day");
     DatePicker paymentDate = new DatePicker("Payment Day");
     RadioButtonGroup<String> radioGroup = new RadioButtonGroup<>();
+    RadioButtonGroup<String> radioGroupCompany = new RadioButtonGroup<>();
     Grid<Product> chooseProduct = new Grid<>(Product.class);
     Grid<Product> productsAdded = new Grid<>(Product.class);
-    Button addItem = new Button("Add");
+    Button addItem = new Button("Add Items to Invoice");
+    Button hint = new Button("Help");
     Paragraph priceToPayField = new Paragraph();
-    Button generateInvoice = new Button("Generate Invoice");
-    Button removeItemsFromGrid = new Button("Remove Items");
+    Button generateInvoice = new Button("Generate Invoice", VaadinIcon.CLIPBOARD_TEXT.create());
     Div component1 = new Div();
     Div component2 = new Div();
+
+    HorizontalLayout layout = new HorizontalLayout();
+    HorizontalLayout layout2 = new HorizontalLayout();
+    HorizontalLayout layout3 = new HorizontalLayout();
+    VerticalLayout layoutFormOne = new VerticalLayout();
+    VerticalLayout layoutFormTwo = new VerticalLayout();
+    VerticalLayout layoutFormThree = new VerticalLayout();
+    VerticalLayout layoutFormFour = new VerticalLayout();
+    HorizontalLayout layoutUnderGrid = new HorizontalLayout();
 
     public InvoiceGenerator(ProductService productService) {
         this.productService = productService;
         addClassName("list-view");
         setSizeFull();
-        HorizontalLayout layout = new HorizontalLayout();
-        layout.setSizeFull();
-        configureDiv();
-        component1.add(searchItems, chooseProduct, addItem, new VerticalLayout(preparedBy, clientName, dateOfPreparation, radioGroup, paymentDate));
-        component2.add(removeItemsFromGrid, productsAdded, generateInvoice, priceToPayField);
+        configureViews();
+        layoutUnderGrid.add(new VerticalLayout( generateInvoice));
+        layoutFormTwo.add(preparedBy, companyLocation, identificationNumberPreparedBy);
+        layoutFormOne.add(clientName, companyLocationOClient, identificationNumber);
+        layoutFormThree.add(radioGroup, dateOfPreparation, paymentDate);
+        layoutFormFour.add(radioGroupCompany);
+        layout2.add(hint);
+        component1.add(searchItems, chooseProduct, addItem, new HorizontalLayout(layoutFormTwo, layoutFormOne, layoutFormFour,layoutFormThree));
+        component2.add(layout2,productsAdded, layoutUnderGrid);
+        layout3.add(invoiceFvNumber);
         layout.add(component1, component2);
         configureButtons();
         configureGenerateButton();
@@ -67,16 +89,67 @@ public class InvoiceGenerator extends VerticalLayout {
         configureGridWithAvailableItems();
         configureGrid();
         configureAddButton();
-        add(layout);
+        generateRandomFv();
+        configureHelpButton();
+        add(layout3, layout, new Footer(new Text("@2021")));
         updateList();
     }
 
 
     private void radioButtons() {
         radioGroup.setLabel("Payment option");
-        radioGroup.setItems("Pay now", "Payment within 14 days");
+        radioGroup.setItems("Pay now", "Payment within 14 days", "Choose other date");
         radioGroup.addThemeVariants(RadioGroupVariant.LUMO_VERTICAL);
-        radioGroup.setValue("Option one");
+        radioGroup.setValue("Pay now");
+        if(radioGroup.getValue().equals("Pay now")){
+            dateOfPreparation.setValue(LocalDate.now());
+            paymentDate.setValue(LocalDate.now());
+            dateOfPreparation.setEnabled(false);
+            paymentDate.setEnabled(false);
+            radioGroup.setRequired(true);
+        }
+        radioGroup.setRequired(true);
+        radioGroupCompany.setLabel("Choose Invoice Format");
+        radioGroupCompany.setItems("Company invoice", "Personal invoice");
+        radioGroupCompany.addThemeVariants(RadioGroupVariant.LUMO_VERTICAL);
+        radioGroupCompany.setValue("Option one");
+        radioGroupCompany.setRequired(true);
+        radioGroupCompany.setValue("Company invoice");
+        if(radioGroup.getValue().equals("Company invoice")){
+            companyLocationOClient.setEnabled(true);
+            identificationNumber.setEnabled(true);
+            companyLocationOClient.setLabel("Company postal address");
+            radioGroupCompany.setRequired(true);
+        }
+        radioGroupCompany.addValueChangeListener(event -> {
+            if (event.getValue().equals("Personal invoice")) {
+                    companyLocationOClient.setLabel("Customer address");
+                identificationNumber.setEnabled(false);
+            } else if (event.getValue().equals("Company invoice")) {
+               companyLocationOClient.setEnabled(true);
+               identificationNumber.setEnabled(true);
+                companyLocationOClient.setLabel("Company postal address");
+            }
+        });
+
+        radioGroup.addValueChangeListener(event -> {
+            if (event.getValue().equals("Pay now")) {
+                dateOfPreparation.setValue(LocalDate.now());
+                paymentDate.setValue(LocalDate.now());
+                dateOfPreparation.setEnabled(false);
+                paymentDate.setEnabled(false);
+            } else if (event.getValue().equals("Payment within 14 days")) {
+                dateOfPreparation.setValue(LocalDate.now());
+                dateOfPreparation.setEnabled(false);
+                paymentDate.setEnabled(false);
+                paymentDate.setValue(LocalDate.now().plusDays(14));
+            } else if (event.getValue().equals("Choose other data")) {
+                dateOfPreparation.setEnabled(true);
+                paymentDate.setEnabled(true);
+                dateOfPreparation.setMin(LocalDate.now());
+            }
+        });
+
     }
 
     private void configureGridWithAvailableItems() {
@@ -101,7 +174,7 @@ public class InvoiceGenerator extends VerticalLayout {
         quantityField.setMax(item.getInStock());
         quantityField.setValue(1d);
         quantityField.addValueChangeListener(e -> {
-                values.put(item.getId(), e.getValue());
+            values.put(item.getId(), e.getValue());
         });
         return quantityField;
     }
@@ -135,7 +208,7 @@ public class InvoiceGenerator extends VerticalLayout {
                 .mapToDouble(Double::valueOf)
                 .sum();
         Paragraph totalPrice = new Paragraph();
-        totalPrice.setText("Total Price: $" +sum);
+        totalPrice.setText("Total Price: $" + sum);
         totalPrice.getStyle().set("font-size", "15px");
         productsAdded.getColumnByKey("Total Price").setFooter(totalPrice);
         return price;
@@ -143,23 +216,27 @@ public class InvoiceGenerator extends VerticalLayout {
 
 
     private void configureAddButton() {
+        addItem.getStyle().set("margin-left","5px");
+        addItem.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
         addItem.addClickListener(click -> {
             chooseProduct.getSelectedItems().forEach(e -> {
                 int findId = e.getId();
                 productList.add(findId);
             });
             productsAdded.setItems(productService.getProductRepository().findAllById(productList));
-
         });
 
     }
 
 
     private void configureGenerateButton() {
-        generateInvoice.getStyle().set("margin-left", "5px");
-        removeItemsFromGrid.getStyle().set("margin-right", "auto");
-        removeItemsFromGrid.addThemeVariants(ButtonVariant.LUMO_ERROR);
+        generateInvoice.addClassName("generate");
+        generateInvoice.setIconAfterText(true);
+        generateInvoice.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        hint.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
     }
+
+
 
     private void configureButtons() {
         searchItems.setPlaceholder("Search products by category, name...");
@@ -170,15 +247,48 @@ public class InvoiceGenerator extends VerticalLayout {
     }
 
 
-    private void configureDiv() {
+    private void configureViews() {
         component2.setWidth("100%");
         component1.setWidth("100%");
+        component2.addClassName("component2");
         component2.setHeightFull();
         component1.setHeightFull();
+        layout.setSizeFull();
+        layout2.setJustifyContentMode(JustifyContentMode.END);
+        invoiceFvNumber.setWidth(null);
+        layout3.setWidth("100%");
+        layout3.setJustifyContentMode(JustifyContentMode.CENTER);
+        layoutUnderGrid.setJustifyContentMode(JustifyContentMode.END);
     }
 
     private void updateList() {
         chooseProduct.setItems(productService.findAll(searchItems.getValue()));
     }
 
+    private void generateRandomFv() {
+        Random random = new Random();
+        invoiceFvNumber.getStyle().set("font-size", "18px");
+        invoiceFvNumber.setText("FV/" + String.valueOf(random.nextInt(Integer.MAX_VALUE)) + "/");
+    }
+
+    private void configureHelpButton(){
+        hint.addClickListener(e->{
+            Dialog dialog = new Dialog();
+
+            H1 h1 = new H1();
+            h1.add("Help");
+            Text text = new Text("Fill up required fields to generate PDF invoice. \n" +
+                    "Old generated invoices can be found in Invoices tab. \n" +
+                    "To add more products go to Products tab, and fill up required form. \n" +
+                    "Good luck :)");
+            dialog.setWidth("500px");
+            dialog.add(h1, text);
+            dialog.open();
+        });
+    }
+
+    private void generatePdfFile(){
+        Document document = new Document();
+
+    }
 }
